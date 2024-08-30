af43bce7135d1b69ef286b94c84c1d2e334599d2
Carlos Henrique <ichfs9108@gmail.com>
2024-08-30 14:20:13 -0300

    Desenvolvimento do CRUD com Flask - Update

--- app.py
+++ app.py
@@ -31,7 +31,25 @@ def get_task(id):
         if t.id == id:
             return jsonify(t.to_dict())
 
-    return jsonify({"message":"Não foi possível encontrar a atividade"}), 404
+    return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
+
+
+@app.route('/tasks/<int:id>', methods=['PUT'])
+def update_task(id):
+    task = None
+    for t in tasks:
+        if t.id == id:
+            task = t
+    print(task)
+    if task == None:
+        return jsonify({"message": "Não foi possível encontrar a atividade"}), 404
+
+    data = request.get_json()
+    task.title = data['title']
+    task.description = data['description']
+    task.completed = data['completed']
+    print(task)
+    return jsonify({"message": "Tarefa atualizada com sucesso"})
 
 if __name__ == "__main__":
     app.run(debug=True)
